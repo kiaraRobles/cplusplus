5e5909bb5d2a6ea653375b47acd9d070b593e8e2
Kiara Robles <kiararobles@gmail.com>
2014-05-19 02:57:12 -0700

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+A simple text-file opening program by Kiara Robles.
+Coded as part of learning C++.
+
+Displays "Hello World" in Latin.
+*/
+
+#include <iostream>
+
+void PlaceCursor(const int x, const int y) 
+{
+	PlaceCursor(25,25);
+	std::cout << "Salve Munde!";
+	return;
+} 
